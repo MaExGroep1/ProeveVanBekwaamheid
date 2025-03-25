27201a64bce666db33ac45fd6396f327bf5e1cc8
braydenbos <bosbrayden@gmail.com>
2025-03-25 10:18:01 +0100

    feature[BlockTypes]: adds block types and a block table

---
+++ Assets/Scripts/Blocks/BlockTypeData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Blocks
+{
+    [CreateAssetMenu(fileName = "BlockTypeData", menuName = "ScriptableObjects/BlockTypeData", order = 1)]
+    public class BlockTypeData : ScriptableObject
+    {
+        public BlockType blockTypes;        // the block type of this data
+        public Sprite blockSprite;          // the icon of this block type
+        public Sprite[] levelSprites;       // the icons of this block type per level
+    }
+}
