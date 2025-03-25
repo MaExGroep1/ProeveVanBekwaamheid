fbd3e5f441a649f2e1aa72a8c8c1111d3c2484ed
stenkristel <stenkristel2016@gmail.com>
2025-03-25 14:17:46 +0100

    doc[userdata]: adds comments to UserData.cs

--- Assets/Scripts/User/UserData.cs
+++ Assets/Scripts/User/UserData.cs
@@ -6,19 +6,23 @@ namespace User
 {
     public class UserData : Util.Singleton<UserData>
     {   
-        private int _score;
-        private float _distanceTraveled;
-        private int _enemiesKilled;
-        private int _enemiesKilledScore;
+        private int _score;                     //The total score of the current run
+        private float _distanceTraveled;        //The distance traveled in the current run
+        private int _enemiesKilled;             //The amount of enemies killed in the current run
+        private int _enemiesKilledScore;        //The amount of points gained by killing enemies in the current run
         
-        public int Score { get => _score; private set => _score = value; }
+        public int Score 
+        {
+            get => _score; 
+            private set => _score = value;
+        }
         public int EnemiesKilledScore { get => _enemiesKilledScore; private set => _enemiesKilledScore = value; }
         public int EnemiesKilled { get => _enemiesKilled; set => _enemiesKilled = value; }
         
         public float DistanceTraveled { get => _distanceTraveled; set => _distanceTraveled = value; }
         
-        public Action<int> OnScoreChange;
-        public Action<int> OnEnemyKilled;
+        public Action<int> OnScoreChange;      //Will increase the score by <int>
+        public Action<int> OnEnemyKilled;      //Invoked whenever an enemy dies, increases the score by <int>
 
 
         private void Awake()
@@ -26,33 +30,48 @@ namespace User
             AssignEvents();
         }
 
+        //Unassigns events onDestroy to avoid null refs
         private void OnDestroy()
         {
             UnAssignEvents();
         }
 
-        private void EnemyKilled(int scoreChange)
+        /// <summary>
+        /// Gets called whenever an enemy dies and updates the scores and enemies killed count
+        /// </summary>
+        /// <param name="scoreChange"></param>
+        private void CountEnemyKill(int scoreChange)
         {
             EnemiesKilled++;
             EnemiesKilledScore += scoreChange; 
             Score += scoreChange;
         }
         
-        private void ScoreChange(int scoreChange)
+        /// <summary>
+        /// Changes the score, by scoreChange amount
+        /// </summary>
+        /// <param name="scoreChange"></param>
+        private void ChangeScore(int scoreChange)
         {
             Score += scoreChange;
         }
         
+        /// <summary>
+        /// assigns events
+        /// </summary>
         private void AssignEvents()
         {
-            OnScoreChange += ScoreChange;
-            OnEnemyKilled += EnemyKilled;
+            OnScoreChange += ChangeScore;
+            OnEnemyKilled += CountEnemyKill;
         }
         
+        /// <summary>
+        /// unassigns events
+        /// </summary>
         private void UnAssignEvents()
         {
-            OnScoreChange -= ScoreChange;
-            OnEnemyKilled -= EnemyKilled;
+            OnScoreChange -= ChangeScore;
+            OnEnemyKilled -= CountEnemyKill;
         }
     }
 }
