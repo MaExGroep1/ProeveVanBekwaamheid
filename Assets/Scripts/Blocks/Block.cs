a6cb2c1bd08dfdd19eb3a9db87d15177ebef8294
Brayden <91124861+braydenbos@users.noreply.github.com>
2025-04-01 12:29:48 +0200

    Feature/match3/matching (#6)
    * feature[BlockTypes]: adds block types and a block table
    
    * feature[Match3/Grid]: adds a grid that generates with blocks of it that are draggable
    
    * feature[Match3/Grid]: adds block falling at start of  the game
    
    * feature[Match3/Grid]: Makes the fall ease in instead of out and makes the grabbed block infront of the other
    
    * comment[Match3/Grid]: adds missing comments to block.cs and gridmanager.cs
    
    * chore[Match3/Grid]: removes unneeded files from .idea and adds .idea to gitignore
    
    * chore[Match3/grid]: Adds scene changes to Match3 scene and removes the SampleScene
    
    * feature[Match3/Matching]: Adds distance check to blocks
    
    * style[Match3/Grid]: Changes placements of the variables in Block and GridElement also adds missing comment in Block
    
    * feature[Match3/Matching]: Adds a distance check to the block input, a spring back and a direction check
    
    * feature[Match3/Mathing]: Adds check for match and point system
    
    * feature[Match3/Matching]: adds match checker that destroys blocks when they are matching in a pair of 3
    
    * style[Match3/Matching]: Adds missing comments and sets the functions in the right order
    
    * feature[Match3/Matching]: adds input block after match and separates fall and move to origin
    
    * comment[Match3/Matching]: Adds missing comments and fixes outdated comments
    
    * comment[Match/Matching]: Fixes the comment

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections;
+using Grid;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Screen = UnityEngine.Screen;
 
 namespace Blocks
 {
@@ -13,34 +16,23 @@ namespace Blocks
         private BlockType _blockType;                   // the block type of the block
         
         private Vector3 _gridPosition;                  // the default position of the block
+        private Vector2Int _cords;                      // the cords in the grid
+
+        private bool _isMoving;                         // whether the block is moving
+        private bool _canMoveWithMouse;                 // whether the block can stick to the mouse
         
         public RectTransform Rect => rect;              // getter of the rect of the grid element
-
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            var parent = transform.parent;
-            transform.SetParent(parent.parent);
-            transform.SetParent(parent);
-        }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-            transform.position = Input.mousePosition;
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            transform.position = _gridPosition;
-        }
         
         /// <summary>
         /// Sets the block type and sprite
         /// </summary>
         /// <param name="data"> the block type data </param>
-        public void Initialize(BlockTypeData data)
+        /// <param name="cords"> the cords of the block</param>
+        public void Initialize(BlockTypeData data, Vector2Int cords)
         {
             _blockType = data.blockTypes;
             image.sprite = data.blockSprite;
+            _cords = cords;
         }
         
         /// <summary>
@@ -52,7 +44,93 @@ namespace Blocks
         /// <summary>
         /// Sets the default position of the block
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="position"> the origin of the block </param>
         public void SetPosition(Vector3 position) => _gridPosition = position;
+        
+        /// <summary>
+        /// Makes the block go to its origin point
+        /// </summary>
+        /// <param name="onComplete"> when the block gets to its origin</param>
+        public void GoToOrigin(Action onComplete)
+        {
+            _canMoveWithMouse = false;
+            _isMoving = true;
+            LeanTween.move(gameObject, _gridPosition, GridManager.Instance.BlockTravelTime).
+                setEase(LeanTweenType.easeInCubic).
+                setOnComplete(()=>StopMoving(onComplete));
+        }
+        
+        /// <summary>
+        /// Makes the block fall to its origin point
+        /// </summary>
+        public void FallToOrigin() => LeanTween.moveY(gameObject, _gridPosition.y, GridManager.Instance.BlockFallTime).setEase(LeanTweenType.easeInCubic);
+        
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if(_isMoving) return;
+            var parent = transform.parent;
+            _canMoveWithMouse = true;
+            transform.SetParent(parent.parent);
+            transform.SetParent(parent);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if(_isMoving || !_canMoveWithMouse) return;
+            transform.position = Input.mousePosition;
+            if (Vector3.Distance(transform.position, _gridPosition) > GridManager.Instance.BlockPlaceDistance * Screen.height / 1920)
+                TryToMatch();
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if(_isMoving) return;
+            if (Vector3.Distance(transform.position, _gridPosition) > GridManager.Instance.BlockSpringBackDistance * Screen.height / 1920)
+            {
+                TryToMatch();
+                return;
+            }
+
+            GoToOrigin(null);
+        }
+        
+        /// <summary>
+        /// Starts the matching process
+        /// </summary>
+        private void TryToMatch()
+        {
+            var direction =  transform.position - _gridPosition;
+            var normalized = direction.normalized;
+            var dir = Mathf.Abs(normalized.x) < Mathf.Abs(normalized.y) ?
+                normalized.y > 0 ? 
+                    Direction.Up: 
+                    Direction.Down: 
+                normalized.x > 0 ? 
+                    Direction.Right: 
+                    Direction.Left;
+
+            GridManager.Instance.TryMatch(_cords, dir, _blockType);
+        }
+        
+        /// <summary>
+        /// Sets the block to immobile 
+        /// </summary>
+        /// <param name="onComplete"> invokes the stop movement</param>
+        private void StopMoving(Action onComplete)
+        {
+            _isMoving = false;
+            onComplete?.Invoke();
+        }
+        
+        /// <summary>
+        /// Destroys the block after a certain time
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public IEnumerator DestroyBlock(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Blocks;
 using UnityEngine;
@@ -18,19 +19,78 @@ namespace Grid
         [SerializeField] private GridElement gridElementTemplate;           // the template of the grid element
         [SerializeField] private Block blockTemplate;                       // the template of the block
         [SerializeField] private BlockTypeTableData blockTypeTableData;     // the available block types
+        [SerializeField] private float blockPlaceDistance;                  // the distance the block can travel before swapping 
+        [SerializeField] private float blockSpringBackDistance;             // the distance the block can travel before not snapping back to its origin
+        [SerializeField] private float blockTravelTime;                     // the time it takes for the block to travel somewhere
+        [SerializeField] private float blockFallTime;                       // the time it takes for the block to fall somewhere
         
         [Header("Spawn animation")]
-        [SerializeField] private float fallTime;                            // the time it takes to fall to the ground
         [SerializeField] private RectTransform gridRect;                    // the rect of the grid object
         [SerializeField] private float gridRectOffset;                      // the offset to add to the fall
         
         private GridElement[,] _grid;                                       // the grid of grid elements
         private Transform _blocksParent;                                    // the parent of all the blocks
-
-
+        
+        public float BlockPlaceDistance => blockPlaceDistance;              // the available block types
+        public float BlockSpringBackDistance => blockSpringBackDistance;    // the available block types
+        public float BlockTravelTime => blockTravelTime;                    // the available block types
+        public float BlockFallTime => blockFallTime;                    // the available block types
+        
         private void Start()
         {
             CreateGrid();
+        }
+        
+        /// <summary>
+        /// Checks if the block has a match
+        /// </summary>
+        /// <param name="cords"> the cords of the block </param>
+        /// <param name="direction"> the direction the block is going to </param>
+        /// <param name="blockType"> the type of the block </param>
+        public void TryMatch(Vector2Int cords, Direction direction, BlockType blockType)
+        {
+            var offset = direction switch
+            {
+                Direction.Up    => new Vector2Int( 1,  0),
+                Direction.Down  => new Vector2Int(-1,  0),
+                Direction.Left  => new Vector2Int( 0, -1),
+                Direction.Right => new Vector2Int( 0,  1),
+                _               => Vector2Int.zero
+            };
+            var otherDir = direction switch
+            {
+                Direction.Up    => Direction.Down,
+                Direction.Down  => Direction.Up,
+                Direction.Left  => Direction.Right,
+                Direction.Right => Direction.Left,
+                _               => direction 
+            };
+
+            var index = cords + offset;
+            
+            var otherType = _grid[index.x, index.y].GetBlockType();
+            int horizontalA = 1 + (direction == Direction.Down  ? 0 : CheckDirection(index, new Vector2Int(1, 0), blockType)) 
+                               + (direction == Direction.Up    ? 0 : CheckDirection(index, new Vector2Int(-1, 0), blockType));
+
+            int verticalA = 1 + (direction == Direction.Right ? 0 : CheckDirection(index, new Vector2Int(0, -1), blockType)) 
+                             + (direction == Direction.Left  ? 0 : CheckDirection(index, new Vector2Int(0, 1), blockType));
+            
+            int horizontalB = 1 + (otherDir == Direction.Down  ? 0 : CheckDirection(cords, new Vector2Int(1, 0), otherType)) 
+                               + (otherDir == Direction.Up    ? 0 : CheckDirection(cords, new Vector2Int(-1, 0), otherType));
+
+            int verticalB = 1 + (otherDir == Direction.Right ? 0 : CheckDirection(cords, new Vector2Int(0, -1), otherType)) 
+                             + (otherDir == Direction.Left  ? 0 : CheckDirection(cords, new Vector2Int(0, 1), otherType));
+
+
+            if (horizontalA <= 2 && verticalA <= 2 && horizontalB <= 2 && verticalB <= 2)
+            {
+                _grid[cords.x, cords.y].GetBlock().GoToOrigin(null);
+                return;
+            }
+            SwapBlocks(cords, index, () => 
+                DestroyAllMatchingBlocks(
+                    index, horizontalA > 2, verticalA > 2,
+                    cords,horizontalB > 2, verticalB > 2));
         }
         
         /// <summary>
@@ -104,27 +164,121 @@ namespace Grid
                     var offset = new Vector3(0,gridRect.rect.height + gridRectOffset ,0);
                     newBlock.Rect.position = position + offset;
                     
-                    newBlock.SetPosition(position);
-
-                    StartCoroutine(WaitToDrop(newBlock.gameObject, position.y, waitTime));
-                
-                    newBlock.Initialize(block);
+                    newBlock.Initialize(block, new Vector2Int(i,j));
                 
                     _grid[i,j].SetBlock(newBlock);
+                    
+                    StartCoroutine(WaitToDrop(newBlock, waitTime));
                 }
+        }
+        
+        /// <summary>
+        /// Checks how many blocks are the same type
+        /// </summary>
+        /// <param name="cords"> the cords to check from </param>
+        /// <param name="direction"> the direction to check </param>
+        /// <param name="blockType"> the type the current block is </param>
+        /// <returns> the amount of blocks of the same type in a row </returns>
+        private int CheckDirection(Vector2Int cords, Vector2Int direction, BlockType blockType)
+        {
+            int i = 1;
+            while (IsWithinBounds(cords + i * direction) && _grid[cords.x + i * direction.x, cords.y + i * direction.y].GetBlockType() == blockType) i++;
+            return i - 1;
+        }
+        
+        /// <summary>
+        /// Checks if the cord is in the bound of the grid
+        /// </summary>
+        /// <param name="cords"> the cords to check from </param>
+        /// <returns> if the cord is in the bound of the grid </returns>
+        private bool IsWithinBounds(Vector2Int cords) => cords.x >= 0 && cords.x < gridHeight && cords.y >= 0 && cords.y < gridWidth;
+        
+        /// <summary>
+        /// Swaps two blocks in the grid 
+        /// </summary>
+        /// <param name="blockAIndex"> the first block </param>
+        /// <param name="blockBIndex"> the second block </param>
+        /// <param name="onComplete"> when the blocks are done moving </param>
+        private void SwapBlocks(Vector2Int blockAIndex, Vector2Int blockBIndex, Action onComplete)
+        {
+            var blockAElement = _grid[blockAIndex.x, blockAIndex.y];
+            var blockBElement = _grid[blockBIndex.x, blockBIndex.y];
+            var blockA = blockAElement.GetBlock();
+            var blockB = blockBElement.GetBlock();
+            
+            blockAElement.SetBlock(blockB);
+            blockBElement.SetBlock(blockA);
+            
+            blockA.GoToOrigin(null);
+            blockB.GoToOrigin(() => onComplete?.Invoke());
+        }
+        
+        /// <summary>
+        /// Destroys all matching blocks near block A and block B
+        /// </summary>
+        /// <param name="cordsA"> the cords of block A </param>
+        /// <param name="horizontalA"> whether to delete on A horizontal </param>
+        /// <param name="verticalA"> whether to delete on A vertical </param>
+        /// <param name="cordsB"> the cords of block B </param>
+        /// <param name="horizontalB"> whether to delete on A horizontal </param>
+        /// <param name="verticalB"> whether to delete on A vertical </param>
+        private void DestroyAllMatchingBlocks(Vector2Int cordsA, bool horizontalA, bool verticalA,Vector2Int cordsB , bool horizontalB, bool verticalB )
+        {
+            DestroyMatchingBlocks(cordsA, _grid[cordsA.x, cordsA.y].GetBlock().GetBlockType(), horizontalA, verticalA);
+            DestroyMatchingBlocks(cordsB, _grid[cordsB.x, cordsB.y].GetBlock().GetBlockType(), horizontalB, verticalB);
+        }
+        
+        /// <summary>
+        /// Destroys all adjacent blocks of the same type
+        /// </summary>
+        /// <param name="cords"> the cords to delete from </param>
+        /// <param name="blockType"> the type of block </param>
+        /// <param name="horizontal"> whether to delete on horizontal </param>
+        /// <param name="vertical"> whether to delete on vertical </param>
+        private void DestroyMatchingBlocks(Vector2Int cords, BlockType blockType, bool horizontal, bool vertical)
+        {
+            if (horizontal)
+            {
+                DestroyBlocksFromDirection(cords, new Vector2Int( 1, 0), blockType);
+                DestroyBlocksFromDirection(cords, new Vector2Int(-1, 0), blockType);
+            }
+            if (vertical)
+            {
+                DestroyBlocksFromDirection(cords, new Vector2Int( 0, 1), blockType);
+                DestroyBlocksFromDirection(cords, new Vector2Int( 0,-1), blockType);
+            }
+
+            if (vertical || horizontal)
+                StartCoroutine(_grid[cords.x, cords.y].GetBlock().DestroyBlock(0.2f));
+            
+        }
+        
+        /// <summary>
+        /// Destroys all blocks of 1 type in a direction from a point
+        /// </summary>
+        /// <param name="cords"> the cord where to delete from </param>
+        /// <param name="direction"> the direction to delete </param>
+        /// <param name="blockType"> the type to delete </param>
+        private void DestroyBlocksFromDirection(Vector2Int cords, Vector2Int direction, BlockType blockType)
+        {
+            int i = 1;
+            while (IsWithinBounds(cords + i * direction) && _grid[cords.x + i * direction.x, cords.y + i * direction.y].GetBlockType() == blockType)
+            {
+                StartCoroutine(_grid[cords.x + i * direction.x, cords.y + i * direction.y].GetBlock().DestroyBlock(0.2f));
+                i++;
+            }
         }
         
         /// <summary>
         /// Waits for a set amount of time then moves the block to the position
         /// </summary>
-        /// <param name="newBlockGameObject"> the block to move</param>
-        /// <param name="to"> the place to move it to </param>
+        /// <param name="newBlock"> the block to move</param>
         /// <param name="waitTime"> the time to wait before falling </param>
         /// <returns></returns>
-        private IEnumerator WaitToDrop(GameObject newBlockGameObject, float to, float waitTime)
+        private IEnumerator WaitToDrop(Block newBlock, float waitTime)
         {
             yield return new WaitForSeconds(waitTime);
-            LeanTween.moveY(newBlockGameObject, to, fallTime).setEase(LeanTweenType.easeInCubic);
+            newBlock.FallToOrigin();
         }
     }
 }
