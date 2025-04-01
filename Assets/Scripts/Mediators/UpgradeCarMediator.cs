6faeb1865a3e7e19bb419932b347b19fba6fef2c
stenkristel <stenkristel2016@gmail.com>
2025-04-01 12:32:15 +0200

    comment[upgradescore]: adds comment to UpgradeMediator

--- Assets/Scripts/Mediators/UpgradeCarMediator.cs
+++ Assets/Scripts/Mediators/UpgradeCarMediator.cs
@@ -4,7 +4,7 @@ using User;
 
 public class UpgradeCarMediator : MonoBehaviour
 {
-    [SerializeField] private BlockType upgradeType;
+    [SerializeField] private BlockType upgradeType;     //The type of upgrade the UI element represents
     
     /// <summary>
     /// Calls upgrade in UpgradeManager and gives upgradeType as upgradeType
