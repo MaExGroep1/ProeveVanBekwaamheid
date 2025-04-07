71c3dce42bb26f34f3fb82c12df668c217b1b978
braydenbos <bosbrayden@gmail.com>
2025-04-07 14:11:26 +0200

    comment[TileSystem]: adds missing comments

--- Assets/Scripts/Tiles/LevelData.cs
+++ Assets/Scripts/Tiles/LevelData.cs
@@ -5,10 +5,9 @@ namespace Tiles
     [CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
     public class LevelData : ScriptableObject
     {
-        public Tile startTile;
-        public Tile[] tiles;
-        public Tile endTile;
-        
-        public Tile RandomTile => tiles[Random.Range(0,tiles.Length)];
+        public Tile startTile;                                          // the start tile of the level
+        public Tile[] tiles;                                            // array of available tiles of the level
+        public Tile endTile;                                            // the end tile of the level
+        public Tile RandomTile => tiles[Random.Range(0,tiles.Length)];  // a random tile from "tiles"
     }
 }
