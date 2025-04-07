c96dddb9f783a3172327f9581a7bdcb7080910f5
Brayden <91124861+braydenbos@users.noreply.github.com>
2025-04-07 14:32:50 +0200

    Feature/tile system (#18)
    * feature[tileSystem]: adds tile system, level data and tile script
    
    * comment[TileSystem]: adds missing comments
    
    * comment[TileSystem]: adds missing comments
    
    * comment[TileSystem]: adds comments to tileloader ontriggerenter

---
+++ Assets/Scripts/Tiles/TileLoader.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace Tiles
+{
+    public class TileLoader : MonoBehaviour
+    {
+        public Action OnLoad; // triggers when near the end of the tile
+        
+        /// <summary>
+        /// Checks if the player collides with the loader
+        /// </summary>
+        /// <param name="other"> The hit object </param>
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            OnLoad?.Invoke();
+        }
+    }
+}
