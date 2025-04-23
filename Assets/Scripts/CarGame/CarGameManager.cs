fed727979ad031837a476981078dd96facd5e654
braydenbos <bosbrayden@gmail.com>
2025-04-23 12:33:59 +0200

    comments[Enemy/Base]: Adds missing comments to CarGameManager

--- Assets/Scripts/CarGame/CarGameManager.cs
+++ Assets/Scripts/CarGame/CarGameManager.cs
@@ -9,9 +9,9 @@ namespace CarGame
     {
         [SerializeField] private LevelData[] levels;                        // all the playable levels
         private int _currentLevelIndex;                                     // the current level of the game
-        public LevelData CurrentLevel => levels[_currentLevelIndex];
+        public LevelData CurrentLevel => levels[_currentLevelIndex];        // the current level of the game
 
-        private Action _onEnterNextLevel;
+        private Action _onEnterNextLevel;                                   // invokes when it gets to a new level
         
         private void Start() => TileManager.Instance.ListenToOnEnterNextLevel(OnEnterNextLevel);
         
@@ -27,9 +27,13 @@ namespace CarGame
         /// <param name="onEnterNextLevel"> the function to remove </param>
         public void StopListeningToOnEnterNextLevel(Action onEnterNextLevel) => _onEnterNextLevel -= onEnterNextLevel;
 
-        
+        /// <summary>
+        /// Sets the next level to the current level +1 or the first level
+        /// </summary>
         private void OnEnterNextLevel()
         {
+            _onEnterNextLevel?.Invoke();
+
             if (_currentLevelIndex < levels.Length - 1)
             {
                 _currentLevelIndex++;
