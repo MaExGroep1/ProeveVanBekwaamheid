5b49aeab4d6c7806f317744f894a51be05658e48
Tomm9000 <90690902+Tomm9000@users.noreply.github.com>
2025-04-23 12:06:13 +0200

    Feature[CarMovement] car now moves based on matches and works with upgrades

--- Assets/Scripts/Car/CarMovement.cs
+++ Assets/Scripts/Car/CarMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Blocks;
+using Car;
 using Grid;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -10,66 +11,70 @@ public class CarMovement : MonoBehaviour
 {
     [SerializeField] private WheelCollider backLeft; // Reference to the Back left wheel
     [SerializeField] private WheelCollider backRight; // Reference to the Back right wheel
-    [SerializeField] private float motorForce = 1500f; // The Motor Force of the vehicle
-    [SerializeField] private float brakeForce = 3000f; // The Brake Force of the vehicle
+    [SerializeField] private float motorForce = 500f; // The Motor Force of the vehicle
+    [SerializeField, Range(0, 1)] private float targetThrottle; // The amount of throttle that needs to be multiplied by the motorforce
+    [SerializeField] private float _maxFuel = 200f; // The Maximum amount of fuel the car has
     
-    [SerializeField, Range(0, 1)] private float targetThrottle = 10f;
-    [SerializeField] private float fuelConsumptionRate = 1f;
+    private bool _hasMatchedBefore; // Check if the player has matched before so they start with max fuel
+    private float _fuel; // The value of the current amount of fuel
     
-    [SerializeField] private float _maxFuel = 100f;
-
-    private float _fuel;
-    
-
-
-    private void Awake()
-    {
+    private void Awake() =>
         GridManager.Instance.ListenToOnMatch(OnMatch);
-    }
-
-
-    private void FixedUpdate() => MoveCar();
     
+    private void FixedUpdate() => 
+        MoveCar();
+    
+    /// <summary>
+    /// Logic that moves the car forward
+    /// </summary>
     private void MoveCar()
-    {
+    { 
+        print(_fuel);
         if (_fuel > 0f)
         {
             ApplyTorque(targetThrottle);
 
-            _fuel -= fuelConsumptionRate * Time.deltaTime;
+            _fuel -= CarData.Instance.FuelContinuousDrain * Time.deltaTime;
             _fuel = Mathf.Max(_fuel, 0f);
         }
-    }
-
-    /// <summary>
-    /// Applies a brakeforce to slow down and stop the vehicle
-    /// </summary>
-    /// <param name="brakeForce">The Amount the vehicle brakes</param>
-    
-    private void ApplyBrake(float brakeForce)
-    {
-        backLeft.brakeTorque = brakeForce;
-        backRight.brakeTorque = brakeForce;
+        else ApplyTorque(0f);
     }
 
     /// <summary>
     /// Applies torque to the wheel collider that allows the car to move forward
     /// </summary>
-    /// <param name="throttle">The Amount to multiply the motorForce by</param>
-
+    /// <param name="throttle"> The Amount to multiply the motorForce by </param>
     private void ApplyTorque(float throttle)
     {
-
-        backLeft.motorTorque = -throttle * motorForce;
-        backRight.motorTorque = -throttle * motorForce;
+        backLeft.motorTorque = throttle * motorForce;
+        backRight.motorTorque = throttle * motorForce;
     }
     
+    /// <summary>
+    /// Calls when the player makes a match 
+    /// </summary>
+    /// <param name="blockType"> The type of the block that has been matched </param>
+    /// <param name="matchAmount"> The amount of blocks that have been matched </param>
     private void OnMatch(BlockType blockType, int matchAmount)
     {
-        _fuel += matchAmount; 
-        _fuel = Mathf.Clamp(_fuel, 0f, _maxFuel);
-        
-        print(_fuel);
+        if (!IsFirstMatch())
+        {
+            _fuel += matchAmount;
+            _fuel = Mathf.Clamp(_fuel, 0f, _maxFuel);
+        }
+        else _fuel = _maxFuel;
     }
-    
+
+    /// <summary>
+    /// Checks if the player made their first match of the "round"
+    /// </summary>
+    /// <returns></returns>
+    private bool IsFirstMatch()
+    {
+        if (_hasMatchedBefore)
+            return false;
+
+        _hasMatchedBefore = true;
+        return true;
+    }
 }
