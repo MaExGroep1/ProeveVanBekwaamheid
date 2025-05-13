e1705f312f1165bfcc9b5206c0db70bef70a76fd
stenkristel <stenkristel2016@gmail.com>
2025-05-13 15:40:20 +0200

    feature[laser-upgrade]: adds laser visual functionality

--- Assets/Scripts/Weapon/LaserWeapon.cs
+++ Assets/Scripts/Weapon/LaserWeapon.cs
@@ -7,33 +7,47 @@ namespace Weapon
 {
     public class LaserWeapon : MonoBehaviour
     {
-        [SerializeField] private float range;
         [SerializeField] private float damage;
         [SerializeField] private float fireTime;
         [SerializeField] private float fireDelay;
-        [SerializeField] private GameObject laserFiringPoint;
-
-        private void Start()
-        {
-            Debug.DrawRay(laserFiringPoint.transform.position, Vector3.forward * range, Color.red, Mathf.Infinity);
-        }
-
+        [SerializeField] private GameObject laser;
+        [SerializeField] private LayerMask laserLayerMask;
+        
+        private bool _canFire;
         private void Update()
         {
-            if (CanFire()) Shoot();
+            if (_canFire) Shoot();
         }
 
         private void Shoot()
         {
-            if (!Physics.Raycast(laserFiringPoint.transform.position, Vector3.forward, out var hitInfo, range)) return;
+            var laserBeam = laser;
+            var rayDirection = laserBeam.transform.right;
+            
+            if (!Physics.Raycast(laserBeam.transform.position, rayDirection, out var hitInfo, Mathf.Infinity))
+            {
+                ScaleLaser(Vector3.zero);
+                return;
+            }
+            ScaleLaser(hitInfo.point);
             if (!hitInfo.collider.gameObject.TryGetComponent(out IDamageable target)) return;
             target.TakeDamage(damage * Time.deltaTime);
         }
 
+        private void ScaleLaser(Vector3 hitPoint)
+        {
+            var laserBeam = laser;
+            Vector3 scale = laserBeam.transform.localScale;
+            float distance = hitPoint != Vector3.zero ? Vector3.Distance(hitPoint, laserBeam.transform.position) : 0f;
+
+            scale.x = distance;
+            laserBeam.transform.localScale = scale;
+        }
+
         private bool CanFire()
         {
-            //todo Make a timer using fireTime and fireDelay
-           return true; 
+           return true;
+           return false;
         }
     }
 }
