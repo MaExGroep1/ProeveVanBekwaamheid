8f13fe30a4e4cc40da42651262ed1fa3e9eebc57
stenkristel <stenkristel2016@gmail.com>
2025-05-14 10:52:11 +0200

    comment[laser-upgrade]: adds comment to Idamageable

--- Assets/Scripts/Interfaces/IDamageable.cs
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -4,6 +4,6 @@ namespace Interfaces
 {
     public interface IDamageable
     {
-        public void TakeDamage(float damage);
+        public void TakeDamage(float damage);   //this function should remove "damage" from a health value
     }
 }
