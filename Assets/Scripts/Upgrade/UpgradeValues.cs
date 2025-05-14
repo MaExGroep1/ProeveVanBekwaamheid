b510cde12504eae670222ce6aa983d62d3de8d5d
stenkristel <stenkristel2016@gmail.com>
2025-05-14 11:11:26 +0200

    feature[laser-upgrade]: remoevs unused varables

--- Assets/Scripts/Upgrade/UpgradeValues.cs
+++ Assets/Scripts/Upgrade/UpgradeValues.cs
@@ -6,7 +6,7 @@ namespace Upgrade
     [Serializable]
     public struct UpgradeValues
     {
-        [Tooltip("The value used in gameplay for this upgrade. The values are: attackSpeedReduction, defenseFuelDrain, fuelContinuousDrain, speed, weaponFireRate")]
+        [Tooltip("The value used in gameplay for this upgrade. The values are: attackSpeedReduction, defenseFuelDrain, fuelContinuousDrain, speed. Weapon stats are handled in the prefab of the weapon")]
         public float newUpgradeValue;       //The value used for every upgrade level to upgrade the car gameplay wise
         public GameObject visuals;          //The visuals that represent an upgrade level
     }
